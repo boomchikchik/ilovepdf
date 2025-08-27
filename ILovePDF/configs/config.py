8649d4b38fa8adf9dbf4728300979a3badbe1bbc
boomchikchik <bulkdeals99@gmail.com>
2025-08-27 23:23:02 +0530

    Update config.py

--- ILovePDF/configs/config.py
+++ ILovePDF/configs/config.py
@@ -11,11 +11,11 @@ from typing import List
 
 class bot(object):
     # get API_ID, API_HASH values from my.telegram.org (Mandatory)
-    API_ID: str = os.environ.get("API_ID")
-    API_HASH: str = os.environ.get("API_HASH")
+    API_ID: str = os.environ.get("API_ID",13216322)
+    API_HASH: str = os.environ.get("API_HASH",15e5e632a8a0e52251ac8c3ccbe462c7)
 
     # add API_TOKEN from @botfather (Mandatory)
-    API_TOKEN: str = os.environ.get("API_TOKEN")
+    API_TOKEN: str = os.environ.get("API_TOKEN",'7335432995:AAE9LR-VEOb4fdfKn3DVxM_F8s7BRtRoMIc')
 
     # def __init__(self):
     #     if not all([self.API_ID, self.API_HASH, self.API_TOKEN]):
